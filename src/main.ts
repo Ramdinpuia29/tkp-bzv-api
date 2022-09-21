e7df08bb8416dc7534317e1b57012109cf73a6ef
Ramdinpuia29 <rdpahmar@gmail.com>
2022-09-21 21:19:50 +0530

    Swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -9,11 +11,20 @@ async function bootstrap() {
 
   app.setGlobalPrefix('/api');
 
+  const config = new DocumentBuilder()
+    .setTitle('TKP Bazarveng Application API')
+    .setDescription('An API Documentation for TKP Bazarveng Application')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api', app, document);
+
   const url = configService.get<string>('app.url');
   const port = configService.get<number>('app.port');
 
   await app.listen(port);
 
   console.log(`⚡ Server running on: http://${url}:${port} ⚡`);
+  console.log(`📄 API Documentation: http://${url}:${port}/api 📄`);
 }
 bootstrap();
